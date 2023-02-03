9336e11c8226fc1976c16aab7065cdcd13ed4fda
Анастасия Боговид <anastaciabogovid@yandex.ru>
2023-02-03 16:26:20 +0300

    Цирк

--- Program.cs
+++ Program.cs
@@ -4,34 +4,7 @@
     public class Program
     {
         public static void Main(string[] args)
-        {
-            List<ICar> cars = new List<ICar>();
 
-            cars.Add(new DodgeViper());
-            cars.Add(new ShelbyS500());
-
-            foreach (var car in cars)
-            {
-                car.Start();
-            }
-
-            foreach (var car in cars)
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    car.ChangeGear();
-                }
-            }
-
-            foreach (var car in cars)
-            {
-                Console.WriteLine(car.CurrentSpeed);
-            }
-
-            foreach (var car in cars)
-            {
-                car.Stop();
-            }
               public class Program
         {
             public static void Main(string[] args)
