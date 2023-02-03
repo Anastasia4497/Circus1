43bbfa4941450cc33cdfc6449682b0f67e4733fe
Анастасия Боговид <anastaciabogovid@yandex.ru>
2023-02-03 16:22:28 +0300

    Цирк

--- Program.cs
+++ Program.cs
@@ -1,10 +1,48 @@
-﻿namespace Circus1
+﻿namespace Classes
 {
-    internal class Program
+ 
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<ICar> cars = new List<ICar>();
+
+            cars.Add(new DodgeViper());
+            cars.Add(new ShelbyS500());
+
+            foreach (var car in cars)
+            {
+                car.Start();
+            }
+
+            foreach (var car in cars)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    car.ChangeGear();
+                }
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.CurrentSpeed);
+            }
+
+            foreach (var car in cars)
+            {
+                car.Stop();
+            }
+              public class Program
+        {
+            public static void Main(string[] args)
+            {
+                Representator representator = new Representator();
+                HrSquad hr = new HrSquad(10);
+                Circus circus = new Circus(representator, hr.HiredActors());
+                circus.StartShow();
+
+                var a = 1;
+
+                Console.WriteLine(++a);
+            }
         }
-    }
-}
